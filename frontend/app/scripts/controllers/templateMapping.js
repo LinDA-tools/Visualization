8fe91327aa6e501d9d044d7bc6027ad647d10f16
migalkin <mikhgalkin@gmail.com>
2014-10-29 20:29:01 +0100

    Added the controllers/templateMapping.js script

---
+++ frontend/app/scripts/controllers/templateMapping.js
@@ -0,0 +1,44 @@
+var templateMapping = function(editObject) {
+    //the input object might be the result of the recommendation algorithm
+    //or the JSON with changed template data, i.e. {tuningOptions:{height:500}}
+
+    var tuningOptions = null;
+    var structureOptions = null;
+
+    //retrieving the fields
+    if (editObject.hasOwnProperty("tuningOptions")) {
+        tuningOptions = editObject["tuningOptions"];
+    }
+
+    if (editObject.hasOwnProperty("structureOptions")) {
+        structureOptions = editObject["structureOptions"];
+    }
+
+    //invoking an appropriate template for a tuning parameter
+    if (tuningOptions !== null) {
+        for (var prop in tuningOptions) {
+            if (tuningOptions.hasOwnProperty(prop)) {
+                if (typeof(tuningOptions[prop]) !== 'object') {
+                    invokeTemplate(prop, tuningOptions[prop]);
+                } else {
+                    var axisOptions = tuningOptions[prop];
+                    for (var axisprop in axisOptions) {
+                        invokeTemplate(axisprop, axisOptions[axisprop]);
+                    }
+                }
+            }
+        }
+    }
+
+    //invoking an appropriate template for a dimension parameter
+    if (structureOptions !== null) {
+        var dimensions = structureOptions['dimensions'];
+        for (var prop in dimensions) {
+            invokeTemplate(prop, dimensions[prop]);
+        }
+    }
+
+};
+
+
+
